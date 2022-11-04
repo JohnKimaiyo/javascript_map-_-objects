cdb1913676839d97e58b339ff0754eded69f62b8
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-04 12:29:41 +0300

    cities

---
+++ project 1 - Copy (2)/cars.js
@@ -0,0 +1,20 @@
+// javascript map
+const map = new Map([
+  [1, "mercedez"],
+  [2, "mazda"],
+  [3, "toyata"],
+  [4, "nissan"],
+  [5, "bmw"],
+  [6, "honda"],
+]);
+
+// javascript object
+const obj = {
+  1: "mercedez",
+  2: "mazda",
+  3: "nisssan",
+  4: "bwm",
+  5: "honda",
+};
+
+console.log({ map, obj });
