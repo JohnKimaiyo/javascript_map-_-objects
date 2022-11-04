9f9f2369fe25436cea9a94d278bf2d58119198b4
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-04 12:00:39 +0300

    javascript object vs maps

--- project 1/month.js
+++ project 1/month.js
@@ -9,12 +9,12 @@ const map = new Map([
 
 // javascript object
 
-const obj = (obje = {
+const obj = {
   1: "jan",
   2: "feb",
   3: "mar",
   4: "apr",
   5: "may",
-});
+};
 
 console.log({ map, obj });
