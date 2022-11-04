c16eb5db65c365c989cc2b6890b4221b68e89a42
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-04 11:37:31 +0300

    javascript maps vs object

---
+++ project 1/countries.js
@@ -0,0 +1,19 @@
+// javascrtpt maps
+const map = new Map ([
+    [1,'kenya'],
+    [2,'japan'],
+    [3,'canada'],
+    [4,'russia'],
+    [5,'sweden']
+]);
+
+// javascript objects
+const obj = {
+    1:'kenya',
+    2:'japan',
+    3:'canada',
+    4:'russia',
+    5:'sweden'
+}
+
+console.log({map, obj});
---
+++ project 1/month.js
@@ -0,0 +1,20 @@
+// javascript map
+const map = new Map([
+  [1, "jan"],
+  [2, "feb"],
+  [3, "mar"],
+  [4, "apr"],
+  [5, "may"],
+]);
+
+// javascript object
+
+const obj = (obje = {
+  1: "jan",
+  2: "feb",
+  3: "mar",
+  4: "apr",
+  5: "may",
+});
+
+console.log({ map, obj });
