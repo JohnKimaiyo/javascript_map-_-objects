097bed6372ae2db5d330cc6f01edf09048a0ab94
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-05 05:53:09 +0300

    team example

---
+++ project 1 - Copy (4)/team.js
@@ -0,0 +1,19 @@
+// javascript map
+const map = new Map ([
+    [1,"manu"],
+    [2,'manicty'],
+    [3,"liverpool"],
+    [4,"chelsea"],
+    [5,"arsena"],
+]);
+
+// javascript onject
+const obj = {
+    1:"manu",
+    2:'mancity',
+    3:'liverpool',
+    4:'chelsea',
+    5:'arsean'
+}
+
+console.log({map, obj});
