176cd43f281e3c724e8c5d00479db0f72e18e487
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-05 10:14:20 +0300

    name example

---
+++ project 1 - Copy (5)/name.js
@@ -0,0 +1,17 @@
+// javascript map
+const map = new Map([
+  [1, "john"],
+  [2, "janet"],
+  [3, "sharon"],
+  [4, "brian"],
+]);
+
+// javascript object
+const obj = {
+  1: "john",
+  2: "janet",
+  3: "sharon",
+  4: "brian",
+};
+
+console.log({ map, obj });
