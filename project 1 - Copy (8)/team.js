cc51d6907ae3a38edbcb4616f7c1ae5d8bde3e20
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-06 05:17:56 +0300

    team example

---
+++ project 1 - Copy (8)/team.js
@@ -0,0 +1,18 @@
+// javascitp map
+const map = new Map([
+    [1,"manu"],
+    [2,"manicty"],
+    [3,"liverpool"],
+    [5,"arsenal"]
+]);
+
+// javascript object
+const obj = {
+    1:"manu",
+    2:'manicity',
+    3:'liverpool',
+    4:'chelsea',
+    5:'arsenals'
+}
+
+console.log({map, obj});
