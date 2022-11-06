d883afaafe6cc6e6f90c2cb7862e63ea57fbd3c5
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-06 04:13:19 +0300

    countries example

---
+++ project 1 - Copy (7)/countries.js
@@ -0,0 +1,20 @@
+// javascript maps
+const map = new Map ([
+    [1,"kenya"],
+    [2,"japan"],
+    [3,'canada'],
+    [4,"russia"],
+    [5,'sweden']
+]);
+
+// javascript objects
+const obj = {
+    1:'kenya',
+    2:'japan',
+    3:'canada',
+    4:'russia',
+    5:'sweden'
+}
+
+console.log({map , obj});
+
