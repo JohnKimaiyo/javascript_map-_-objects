67ac997b6f05e044f3717e499ccd550c7b500583
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-06 13:49:24 +0300

    cities example

---
+++ project 1 - Copy (10)/cities.js
@@ -0,0 +1,19 @@
+// javascript map
+const map = new Map ([
+    [1,"nairobi"],
+    [2,"london"],
+    [3,"chicago"],
+    [4,"kampala"],
+    [5,"tokyo"]
+]);
+
+// javascript objects
+const obj ={
+    1:'naironi',
+    2:'london',
+    3:'chicago',
+    4:'kamplala',
+    5:'tokyo'
+}
+
+console.log({map, obj});
