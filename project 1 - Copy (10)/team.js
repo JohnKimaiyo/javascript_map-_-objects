8ca2018718365d892cf0535431ec0875e4ca0e47
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-06 14:12:24 +0300

    team example

---
+++ project 1 - Copy (10)/team.js
@@ -0,0 +1,19 @@
+// javasrcipt map 
+const map = new Map ([
+    [1,"manu"],
+    [2,"mancity"],
+    [3,"liverpool"],
+    [4,"chelsea"],
+    [5,"arsenal"]
+]);
+
+// javascript object
+const obj ={
+    1:'manu',
+    2:'manicity',
+    3:'liverpool',
+    4:'cheleas',
+    5:'aresenal'
+};
+
+console.log({map , obj});
