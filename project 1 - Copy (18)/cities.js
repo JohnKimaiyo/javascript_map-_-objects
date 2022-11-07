4a8eafa0ed598ec335e645607c221310aef8f24e
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 13:11:08 +0300

    cities example

---
+++ project 1 - Copy (18)/cities.js
@@ -0,0 +1,19 @@
+// javascript map
+const map = new Map ([
+    [1,"london"],
+    [2,"nairobi"],
+    [3,"kamapla"],
+    [4,"austin"],
+    [5,"lisbon"]
+]);
+
+// javascript object
+const obj ={
+    1:'london',
+    2:'nairobi',
+    3:'austin',
+    4:'lisbon',
+    5:'kamapla'
+}
+
+console.log({map, obj});
---
+++ project 1 - Copy (18)/money.js
@@ -0,0 +1,19 @@
+// javascript map
+const map = new Map([
+  [1, "yen"],
+  [2, "shilling"],
+  [3, "dollar"],
+  [4, "euro"],
+  [5, "aussie"],
+]);
+
+// javasript objects
+const obj = {
+  1: "yen",
+  2: "shilling",
+  3: "dollar",
+  4: "euro",
+  5: "asussie",
+};
+
+console.log({ map, obj });
