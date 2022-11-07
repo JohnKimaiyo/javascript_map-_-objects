712c7b015543c97867a718f4992626fb1e0cf04a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 13:31:15 +0300

    developer example

---
+++ project 1 - Copy (18)/developer.js
@@ -0,0 +1,19 @@
+// javascript map
+const map = new Map ([
+    [1,"react developer"],
+    [2,"node developer"],
+    [3,"django developer"],
+    [4,"angular developer"],
+    [5,"java developer"]
+]);
+
+// javascript object
+const obj ={
+    1:'react developer',
+    2:'node developer',
+    3:'django developer',
+    4:'angular developer',
+    5:'java developer'
+}
+
+console.log({map, obj});
