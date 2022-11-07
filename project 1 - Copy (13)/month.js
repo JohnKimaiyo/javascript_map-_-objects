56ca80978ac9e705fc9b12b0f37eb7a53154c2f3
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 07:50:55 +0300

    month example

---
+++ project 1 - Copy (13)/month.js
@@ -0,0 +1,19 @@
+// javascript map
+const map = new Map ([
+    [1,"janaury"],
+    [2,"february"],
+    [3,"march"],
+    [4,"april"],
+    [5,"may"]
+])
+
+//javascript object
+const obj = {
+    1:'january',
+    2:'february',
+    3:'march',
+    4:'april',
+    5:'may',
+}
+
+console.log({map, obj});
