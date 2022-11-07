0c224e0b52bb58015ab397788b0ae342f24cc737
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 09:52:12 +0300

    git add .

---
+++ project 1 - Copy (16)/cities.js
@@ -0,0 +1,18 @@
+// javascript map
+const map = new Map([
+  [1, "london"],
+  [2, "berlin"],
+  [3, "tokyo"],
+  [4, "kampala"],
+  [5, "nairbi"],
+]);
+
+// javascript object
+const obj = {
+  1: "london",
+  2: "berlin",
+  3: "tokyo",
+  4: "kamapla",
+  5: "nairobi",
+};
+console.log({ map, obj });
