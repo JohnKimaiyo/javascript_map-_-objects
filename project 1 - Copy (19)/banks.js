e0b83dd9cc2a794a9a2beac55b7a0bd75ba5c6c7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 13:41:10 +0300

    banks example

---
+++ project 1 - Copy (19)/banks.js
@@ -0,0 +1,18 @@
+const map = new Map ([
+    [1,"chase bank"],
+    [2,"city bank"],
+    [3,"barclays bank"],
+    [4,"equity back"],
+    [5,"eco bank"],
+]);
+
+// javascript object
+const obj ={
+    1:"chase bank",
+    2:'city bank',
+    3:'barclays bank',
+    4:'equity bank',
+    5:'eco bank'
+}
+
+console.log({map,obj})
