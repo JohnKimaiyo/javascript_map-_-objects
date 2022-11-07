dfaa24b37784077c22edcafc360f45a504340e5c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 15:13:16 +0300

    bank example

---
+++ project 1 - Copy (20)/bank.js
@@ -0,0 +1,19 @@
+//javascript map
+const map = new Map([
+  [1, "chase bank"],
+  [2, "morgan stanely bank"],
+  [3, "goldman sacks bank"],
+  [4, "city bank"],
+  [5, "barclays bank"],
+]);
+
+// javascript obj
+const obj = {
+  1: "chase bank",
+  2: "morgan stanely bank",
+  3: "goldman sacks bank",
+  4: "city bank",
+  5: "barclays bank",
+};
+
+console.log({ map, obj });
