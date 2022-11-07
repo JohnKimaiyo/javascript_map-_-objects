4db7cd3577e47b710df7e8575e8572d6e2766658
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 11:12:58 +0300

    final commit

---
+++ project 1 - Copy (17)/name.js
@@ -0,0 +1,19 @@
+// javasxcript map
+const map = new Map([
+  [1, "mary"],
+  [2, "john"],
+  [3, "jeff"],
+  [4, "james"],
+  [5, "joan"],
+]);
+
+// javascript objects
+const obj = {
+  1: "mary",
+  2: "john",
+  3: "jeff",
+  4: "james",
+  5: "joan",
+};
+
+console.log({ map, obj });
