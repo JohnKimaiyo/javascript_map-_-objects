5e79884559370af7ea7822b39ac0eb9a171e2e6f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 15:25:10 +0300

    dev example

---
+++ project 1 - Copy (20)/dev.js
@@ -0,0 +1,18 @@
+// javascript map
+const map = new Map([
+  [1, "react developer"],
+  [2, "vue developer"],
+  [3, "angular developer"],
+  [4, "django developer"],
+  [5, "flask developer"],
+]);
+
+// javascript object
+const obj = {
+  1: "react developer",
+  2: "vue developer",
+  3: "angular developer",
+  4: "django developer",
+  5: "flask developer",
+};
+console.log({ map, obj });
