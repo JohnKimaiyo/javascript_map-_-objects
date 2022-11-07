90dbce503a230c6a1be0c4475f78f0407da15dcd
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 13:55:41 +0300

    investor example

---
+++ project 1 - Copy (19)/investor.js
@@ -0,0 +1,19 @@
+// javascript map
+const map = new Map([
+  [1, "elon musk"],
+  [2, "warren buffet"],
+  [3, "bill gates"],
+  [4, "patrich david"],
+  [5, "john rockefellor"],
+]);
+
+// javascript map
+const obj = {
+  1: "elon musk",
+  2: "warren buffet",
+  3: "bill gates",
+  4: "patrcik david",
+  5: "john rockerfellor",
+};
+
+console.log({ map, obj });
