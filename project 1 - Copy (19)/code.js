90cbb610c6b7b06cafc207507db8921b8f142259
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 14:01:04 +0300

    code example

---
+++ project 1 - Copy (19)/code.js
@@ -0,0 +1,19 @@
+// javascript map
+const map = new Map([
+  [1, "javascript"],
+  [2, "python"],
+  [3, "java"],
+  [4, "scala"],
+  [5, "golang"],
+]);
+
+// javascript object
+const obj = {
+  1: "javascript",
+  2: "python",
+  3: "java",
+  4: "scala",
+  5: "golang",
+};
+
+console.log({ map, obj });
