cdf5b67debf9a0428c063193190b74af91b1ab69
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 10:42:12 +0300

    countries map

---
+++ project 1 - Copy (17)/countries.js
@@ -0,0 +1,19 @@
+// javascript map
+const map = new Map ([
+    [1,"kenya"],
+    [2,"uganda"],
+    [3,'somalia'],
+    [4,"zambia"],
+    [5,"ethiopia"]
+]);
+
+// javascript obect
+const obj = {
+    1:'kenya',
+    2:'uganda',
+    3:'smalia',
+    4:'zambia',
+    5:'ethiopia'
+}
+
+console.log({map, obj});
