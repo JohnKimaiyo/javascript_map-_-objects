3c0f047afc75d2c99fdb26c347cfa87626191320
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-07 10:29:14 +0300

    cars example

---
+++ project 1 - Copy (17)/cars.js
@@ -0,0 +1,19 @@
+// javascript map
+const map = new Map([
+  [1, "mercedez"],
+  [2, "honda"],
+  [3, "bmw"],
+  [4, "toyota"],
+  [5, "jeep"],
+]);
+
+// javasstcipt object
+const obj = {
+  1: "mercedez",
+  2: "honda",
+  3: "bmw",
+  4: "toyota",
+  5: "jeep",
+};
+
+console.log({ map, obj });
